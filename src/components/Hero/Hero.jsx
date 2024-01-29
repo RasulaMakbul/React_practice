c14d9afa6641959648f3dde4a155fc4c6ef43e32
RasulaMakbul <emu.cfs@gmail.com>
2024-01-29 10:06:32 +0600

    hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import Image3 from "../../assets/image3.jpg"
+import Image2 from "../../assets/image2.jpg"
+import Image1 from "../../assets/image1.png"
+
+const ImageList = [
+  {
+    id: 1,
+    img: Image1,
+    title: "Hero 1 Title 1",
+    description:
+      "I have a cure for insomnia. Its probably worth millions of dollars but Im giving it to you free. It isnt warm milk or chamomile tea. Its list making. List stuff from your life like all your teachers or all the live music youve seen or all the Paul Newman movies youve watched",
+  },
+  {
+    id: 2,
+    img: Image2,
+    title: "Hero 2 Title 2",
+    description:
+      "I have a cure for insomnia. Its probably worth millions of dollars but Im giving it to you free. It isnt warm milk or chamomile tea. Its list making. List stuff from your life like all your teachers or all the live music youve seen or all the Paul Newman movies youve watched",
+  },
+  {
+    id: 3,
+    img: Image3,
+    title: "Hero 3 Title 3",
+    description:
+      "I have a cure for insomnia. Its probably worth millions of dollars but Im giving it to you free. It isnt warm milk or chamomile tea. Its list making. List stuff from your life like all your teachers or all the live music youve seen or all the Paul Newman movies youve watched",
+  },
+]
+
+const Hero = () => {
+  return (
+    <>
+      <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 ">
+        {/* background pattern */}
+        <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9 "></div>
+        {/* Hero Section */}
+        <div className="container pb-8 sm:pb-0">
+          <div className="">
+            <div className="grid grid-cols-1 sm:grid-cols-2">
+              {/* text content section */}
+              {/* text content section */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Hero
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,59 @@
 import React from "react"
 import logo from "./../../assets/logo.jpg"
+import {IoMdSearch} from "react-icons/io"
+import {FaCaretDown, FaCartShopping} from "react-icons/fa6"
+import DarkMode from "./DarkMode"
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 4,
+    name: "HomMens Wears",
+    link: "/#",
+  },
+  {
+    id: 5,
+    name: "Womens",
+    link: "/#",
+  },
+  {
+    id: 6,
+    name: "Electronics",
+    link: "/#",
+  },
+]
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
+  },
+]
 
 const Navbar = () => {
   return (
@@ -13,20 +67,71 @@ const Navbar = () => {
               React Ecom
             </a>
           </div>
-          {/* search bar and order button */}
-          <div className="">
-            <div className="group">
+          {/* search bar  */}
+          <div className="flex justify-between items-center gap-4 py">
+            <div className="relative group hidden sm:block">
               <input
                 type="text"
                 placeholder="Search"
-                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary"
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800"
               />
+              <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
+            </div>
+            {/* order button */}
+            <button
+              onClick={() => alert("Order not available yet!")}
+              className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group"
+            >
+              <span className="group-hover:block hidden transition-all duration-200">
+                Order
+              </span>
+              <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+            </button>
+            {/* Darkmode Button */}
+            <div className="">
+              <DarkMode />
             </div>
           </div>
         </div>
       </div>
       {/* Lower Navbar */}
-      <div className=""></div>
+      <div className=" flex justify-center">
+        <ul className="sm:flex hidden items-center gap-4 ">
+          {Menu.map(data => (
+            <li key={data.id}>
+              <a
+                href={data.link}
+                className="incline-block px-4 hover:text-primary duration-200 "
+              >
+                {data.name}
+              </a>
+            </li>
+          ))}
+          {/* simple dropdown and links */}
+          <li className="group relative cursor-pointer">
+            <a href="#" className="flex items-center gap-[2px] py-2">
+              Trending
+              <span>
+                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+              </span>
+            </a>
+            <div className=" absolute z-[9999] hidden group-hover:block w-[150px] rounded-md bg-white p-2 text-black">
+              <ul>
+                {DropdownLinks.map(data => (
+                  <li key={data.id}>
+                    <a
+                      href={data.link}
+                      className="inline-block w-full rounded-md p-2 hover:bg-primary/20 "
+                    >
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
