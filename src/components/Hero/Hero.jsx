44f76197dea5dcf4899024c102dfb5627cc86cfa
RasulaMakbul <emu.cfs@gmail.com>
2024-01-30 09:49:01 +0600

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Image3 from "../../assets/image3.jpg"
 import Image2 from "../../assets/image2.jpg"
 import Image1 from "../../assets/image1.png"
+import Slider from "react-slick"
 
 const ImageList = [
   {
@@ -28,6 +29,25 @@ const ImageList = [
 ]
 
 const Hero = () => {
+  // var settings = {
+  //   dots: false,
+  //   arrows: false,
+  //   Infinite: true,
+  //   speed: 800,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 4000,
+  //   cssEase: "ease-in-out",
+  //   pauseOnHover: false,
+  //   pauseOnFocus: true,
+  // }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
   return (
     <>
       <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 ">
@@ -35,22 +55,37 @@ const Hero = () => {
         <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9 "></div>
         {/* Hero Section */}
         <div className="container pb-8 sm:pb-0">
-          <div className="">
-            <div className="grid grid-cols-1 sm:grid-cols-2">
-              {/* text content section */}
-              <div className="">
-                <h1>Lorem ipsum dolor sit.</h1>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex,
-                  modi?
-                </p>
-                <div className="">
-                  <button className="">Order Now </button>
+          <Slider {...settings}>
+            <div className="">
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text content section */}
+                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                  <h1 className="text-3xl sm:text-6xl lg:text-7xl font-bold ">
+                    Lorem ipsum dolor sit.
+                  </h1>
+                  <p className="text-sm">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Ex, modi?
+                  </p>
+                  <div className="">
+                    <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                      Order Now{" "}
+                    </button>
+                  </div>
+                </div>
+                {/* Image section */}
+                <div className="order-1 sm:order-2">
+                  <div className="relative z-10">
+                    <img
+                      src={Image3}
+                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto "
+                      alt=""
+                    />
+                  </div>
                 </div>
               </div>
-              {/* text content section */}
             </div>
-          </div>
+          </Slider>
         </div>
       </div>
     </>
